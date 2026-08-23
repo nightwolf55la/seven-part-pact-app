f71a8852a6a133f43323310d74caeb29fb383071
nightwolf55la <nightwolf55la@gmail.com>
2026-08-22 21:35:51 -0500

    Downgrade Next.js, fix ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from "eslint/config";
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
-import nextTypescript from "eslint-config-next/typescript";
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import convexPlugin from "@convex-dev/eslint-plugin";
 
-export default defineConfig([
-  ...nextCoreWebVitals,
-  ...nextTypescript,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...convexPlugin.configs.recommended,
-]);
+];
